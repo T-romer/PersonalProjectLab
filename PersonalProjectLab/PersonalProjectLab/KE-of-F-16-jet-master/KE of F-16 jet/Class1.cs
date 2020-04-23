30baac6db233480b14e6c08f8e207ac1bd1fbccb
T-romer <59981890+T-romer@users.noreply.github.com>
2020-04-23 15:59:36 -0500

    Add files via upload

---
+++ PersonalProjectLab/PersonalProjectLab/KE-of-F-16-jet-master/KE of F-16 jet/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KE_of_F_16_jet
+{
+    public class Class1
+    {
+        List<int> _velocity;
+        public Class1(List<int> velocity)
+        {
+            _velocity = velocity;
+        }
+
+        public int VelocitiesAboveThreshold(int threshold)
+        {
+            int countofvel = 0;
+            foreach (int velocity in _velocity)
+            {
+                if (velocity > threshold)
+                {
+                    countofvel += 1;
+                }
+            }
+            return countofvel;
+        }
+
+        public int VelocitiesBelowThreshold(int threshold)
+        {
+            int velCount = 0;
+            foreach (int velocity in _velocity)
+            {
+                if (velocity < threshold)
+                {
+                    velCount += 1;
+                }
+            }
+            return velCount;
+        }
+
+        public int VelocitiesAtThreshold(int threshold)
+        {
+            int countVel = 0;
+            foreach (int velocity in _velocity)
+            {
+                if (velocity == threshold)
+                {
+                    countVel += 1;
+                }
+            }
+            return countVel;
+        }
+    }
+}
---
+++ PersonalProjectLab/PersonalProjectLab/KE-of-F-16-jet-master/KE of F-16 jet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KE_of_F_16_jet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kinetic Energy of F-16 for a sonic boom");
+
+            int velocity;
+            double KE;
+            string input = "";
+
+            // Get the jet velocity from the user
+            Console.WriteLine("Enter the jet velocity");
+
+            input = Console.ReadLine();
+
+            velocity = int.Parse(input);
+
+            // Calculate the Kinetic Energy
+
+            KE = (9280 * velocity * velocity) / 2;
+
+            Console.WriteLine("The Kinetic Energy is " + KE);
+        }
+    }
+}
